808f7f1710f798216e7cbff7a2ebd153ef624829
Kaveesha <kaveeshewaduge111@gmail.com>
2024-09-28 20:29:29 +0530

    CoinDtos

---
+++ Auction_Web_App/Dto/Coin/CoinDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Auction_Web_App.Dto.Coin
+{
+    public class CoinDto
+    {
+        public int Id { get; set; }
+        public string Lot { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public decimal CurrentMarketPrice { get; set; }
+        public string Condition { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StyleId { get; set; }
+        public int? MediumId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+    }
+}
